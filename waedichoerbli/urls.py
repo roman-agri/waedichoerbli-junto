2cff5c3d771a4f1e6fef8c0ae02bbcdce973f124
roman-agri <80098243+roman-agri@users.noreply.github.com>
2021-05-02 17:27:32 +0200

    Update urls.py

--- waedichoerbli/urls.py
+++ waedichoerbli/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^', include('juntagrico.urls')),
     url(r'^$', juntagrico.views.home),
     url(r'^impersonate/', include('impersonate.urls')),
+    path(r'', include('juntagrico_assignment_request.urls')),
 ]
-path(r'', include('juntagrico_assignment_request.urls')),
+
