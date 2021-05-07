3d90ddcb64786b42841f86bda3982cf7ea62c537
Erdmut <83092009+Erdmut@users.noreply.github.com>
2021-05-07 14:40:26 +0200

    Update urls.py

--- waedichoerbli/urls.py
+++ waedichoerbli/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'', include('juntagrico.urls')),
     path(r'', juntagrico.views.home),
-    path(r'^impersonate/', include('impersonate.urls')),
+    path(r'impersonate/', include('impersonate.urls')),
     path(r'', include('juntagrico_assignment_request.urls')),
 ]
 
