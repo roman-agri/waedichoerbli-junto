c696689d11731a6d82583a964422fe753c33a2dc
Patricia Portmann <eugster@treibauf.ch>
2021-08-19 11:09:38 +0200

    corrections analog other sites

--- waedichoerbli/urls.py
+++ waedichoerbli/urls.py
@@ -1,6 +1,6 @@
 """waedichoerbli URL Configuration
 
-The `urlpatterns` list routes URLs to views. 
+The `urlpatterns` list routes URLs to views.
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,15 +12,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.conf.urls import include, url
 from django.contrib import admin
 import juntagrico
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'', include('juntagrico.urls')),
-    path(r'', juntagrico.views.home),
-    path(r'impersonate/', include('impersonate.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('juntagrico.urls')),
+    url(r'^$', juntagrico.views.home),
+    url(r'^impersonate/', include('impersonate.urls')),
 ]
-
-
